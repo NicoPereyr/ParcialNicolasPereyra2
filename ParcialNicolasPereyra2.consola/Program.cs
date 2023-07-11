93103ca2ed65a897ac7dc331d8de5cc25ad2ba56
NicoPereyr <nicolaspereyra33@gmail.com>
2023-07-11 20:00:20 -0300

    Agregar archivos de proyecto.

---
+++ ParcialNicolasPereyra2.consola/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialNicolasPereyra2.consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            {
+                double area, volumen, h, l, arista;
+                int contador, cantidadPiramides;
+                cantidadPiramides = 0;
+
+                do
+                {
+                    Console.Write("Ingrese el valor de la altura: ");
+                    h = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Ingrese el valor de la base: ");
+                    l = Convert.ToDouble(Console.ReadLine());
+
+                    area = CalcularArea(h, l);
+                    arista = CalcularArista(h, l);
+                    volumen = CalcularVolumen(h, l);
+                    cantidadPiramides++;
+
+                    Console.WriteLine("Área= {0} Volumen= {1} Arista= {2}", area, volumen, arista);
+                    Console.WriteLine("La cantidad de piramides ingresadas es: {0}", cantidadPiramides);
+            }
+                while (h != 0 && l != 0);
+
+            
+            }
+
+            private static double CalcularArea(double altura, double baseTriangulo)
+            {
+                return (baseTriangulo * baseTriangulo) + (2 * baseTriangulo * altura);
+            }
+
+            private static double CalcularArista(double altura, double baseTriangulo)
+            {
+                return Math.Sqrt((baseTriangulo * baseTriangulo) + (altura * altura));
+            }
+
+            private static double CalcularVolumen(double altura, double baseTriangulo)
+            {
+                return (baseTriangulo * baseTriangulo * altura) / 3;
+            }
+        }
+
+    }
+
+
---
+++ ParcialNicolasPereyra2.windows/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ParcialNicolasPereyra2.windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIngresoAltura = new System.Windows.Forms.TextBox();
+            this.txtIngresoBase = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese el valor de la altura:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ingrese el valor de la base:";
+            // 
+            // txtIngresoAltura
+            // 
+            this.txtIngresoAltura.Location = new System.Drawing.Point(246, 15);
+            this.txtIngresoAltura.Name = "txtIngresoAltura";
+            this.txtIngresoAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtIngresoAltura.TabIndex = 1;
+            // 
+            // txtIngresoBase
+            // 
+            this.txtIngresoBase.Location = new System.Drawing.Point(246, 57);
+            this.txtIngresoBase.Name = "txtIngresoBase";
+            this.txtIngresoBase.Size = new System.Drawing.Size(100, 20);
+            this.txtIngresoBase.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(454, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(454, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtIngresoBase);
+            this.Controls.Add(this.txtIngresoAltura);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIngresoAltura;
+        private System.Windows.Forms.TextBox txtIngresoBase;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
